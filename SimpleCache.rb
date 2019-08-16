884aac7898941fa8ff21f6225c49c59a4bf896e5
Gabriel Duarte <39168030+gjduart@users.noreply.github.com>
2019-08-16 10:26:36 -0300

    Update SimpleCache.rb

--- SimpleCache.rb
+++ SimpleCache.rb
@@ -29,7 +29,7 @@ puts
 puts "Insira o Tamanho da Cache"
 tamanho_cache       = gets.to_i
 tamLinhaCache       = 16
-set_line						 = 4
+set_line	    = 4
 enderecoMemoria     = 32
 politicaDeSubs      = 0
 unidadedeTamanho    = 10
@@ -71,7 +71,7 @@ mem_req = 0       #Requisição da Memoria
 class CacheLine
   		valida = 1.to_i
   		tag    = 0.to_i
-    	ultimo_acesso = 0.to_i
+    		ultimo_acesso = 0.to_i
    		nrAcesso = 0.to_i
 
 		attr_reader :valida, :tag, :ultimo_acesso, :nrAcesso  
@@ -91,8 +91,8 @@ end
 
 
 	 maskTag = 0
-   maskIndex = 0
-   mask = 0
+ 	 maskIndex = 0
+         mask = 0
 		for i in 0..indexBits-1                        # Aqui temos o inicio das nossas operacoes bit a bit
 			 maskIndex<<=1                                    
 			 maskIndex |= 0x01                                          
@@ -126,13 +126,13 @@ linhaBloco = []
 
 #Mapeamento DIRETO! 
 
-for lines in 0..tamTrace.length-1                          #Faz um for por todas as linhas do arquivo
+for lines in 0..tamTrace.length-1                         	 #Faz um for por todas as linhas do arquivo
    mem_req += 1 																				   #Um pequeno Contador
 	 check = HexaParaDec(tamTrace[lines])                    #pega a String na posicao Line do Trace e Transforma pra Decimal
 	 p = (check & maskIndex.to_i) >> offsetBits              #da um and do check e da mascara e da o shift rigth para ter a index
 	
 	 if cache[p].valida == 0                                 #Verifica se o index da linha da cache e valida
-	 	    cache[p].tag = check  & maskTag.to_i               #define uma nova tag a partir do Check em hexa e da mascara da tag
+	 	    cache[p].tag = check  & maskTag.to_i         #define uma nova tag a partir do Check em hexa e da mascara da tag
 				cache[p].valida = 1                                #define que e valida
 				missDireto += 1                                    #da um cache Miss
 				
